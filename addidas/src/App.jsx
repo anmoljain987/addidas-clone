9290e936c57fd6670723ca4b7f7ac6347092cbdc
Venkateshkp <venkatpandu938@gmail.com>
2022-04-08 23:54:23 +0530

    made some changes on kids and men page

--- addidas/src/App.jsx
+++ addidas/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
-import Kids from "./Components/Kids/Kids.jsx";
+// import Kids from "./Components/Kids/Kids.jsx";
+import Men from "./Components/Men/Men.jsx";
 function App() {
   return (
     <div className="App">
-      <Kids />
+      {/* <Kids /> */}
+      <Men />
     </div>
   )
 }
--- addidas/src/Components/Men/Men.jsx
+++ addidas/src/Components/Men/Men.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 // import React, { useRef, useState } from "react";
 
-import "./Kidsstyles.css";
+import "./Menstyles.css";
+import ReactPlayer from 'react-player';
 // import Slider from "react-slick";
 // import "slick-carousel/slick/slick.css";
 // import "slick-carousel/slick/slick-theme.css";
 
-export default function Kids() {
+export default function Men() {
   // let settings = {
   //   dot: true,
   //   infinite: true,
@@ -18,84 +19,92 @@ export default function Kids() {
   return (
     <div>
       <div>
-        <img src='https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/if_w_gt_1920,w_1920/enIN/Images/adidas-lego-originals-ss22-flex-launch-clp-masthead-d_tcm209-823156.jpg' className="bground"></img>
-        <img src='https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/enIN/Images/H23161_BOS_LOGO_lockup_white_tcm209-816446.png' className="logo"></img>
-        <h1 className='text'>IMAGINE YOUR WORLD</h1>
-        <p className='text1'>Turn on your imagination and play with the new exclusive adidas LEGO® Originals collection. Now available.</p>
+      <ReactPlayer
+            playing={true}
+            loop={true}
+            muted={true}
+            width="100%"
+            height="100%"
+            url="https://brand.assets.adidas.com/video/upload/q_auto,vc_auto/video/upload/running-ss22-ultraboost-launch-hp-masthead-large-3d-t_ow4fcp.mp4"
+          />
+        {/* <video src='https://brand.assets.adidas.com/video/upload/q_auto,vc_auto/video/upload/running-ss22-ultraboost-launch-hp-masthead-large-3d-t_ow4fcp.mp4' autoPlay="autoPlay" typeof='video/mp4' className='bground'></video> */}
+        <h1 className='text'>ULTRABOOST 22</h1>
+        <p className='text1'>Supreme energy return meets 360 fit improvement. Own your energy and achieve your impossible</p>
         <button className='button'>Shop Now &#8594;</button>  
       </div>
       {/* <Slider {...settings}>  */}
+      <h1 className='text2'>MEN ORIGINALS</h1>
       <div className='flex-outside'>
         <div className='flex-inside'>
           <div className='border'>
-            <img src='https://assets.adidas.com/images/w_276,h_276,f_auto,q_auto,fl_lossy,c_fill,g_auto/0a1b2a26c2b0449d928eacae0129b4aa_9366/adidas_x_LEGO(r)_DOTStm_Short_Tights_Black_GP1144_01_laydown.jpg' alt="shoes"></img>
-            <p>adidas x LEGO® DOTS™ Short Tights <br></br><span>Girls Lifestyle</span></p>
+            <img src='https://assets.adidas.com/images/w_276,h_276,f_auto,q_auto,fl_lossy,c_fill,g_auto/8636ee2b569e461db43bae0b00e3caa6_9366/NMD_V3_Shoes_Black_GW3064_01_standard.jpg' alt="shoes"></img>
+            <p>NMD_V3 Shoes <br></br><span>Men Originals</span><br></br>new</p>
             <p className='dis-percentage'>-60%</p>
             <p className='discount-tag'><span className='org-price'>₹2499.00</span><span className='dis-price'>₹1000.00</span></p>
           </div >
           <div className='border'>
-            <img src='https://assets.adidas.com/images/w_276,h_276,f_auto,q_auto,fl_lossy,c_fill,g_auto/19d89a1733954702aecbacc901752adc_9366/adidas_ZX_8000_x_LEGO(r)_Shoes_Multicolor_GZ8215_01_standard.jpg' alt='shoes'></img>
-            <p>adidas ZX 8000 x LEGO® Shoes <br></br><span>Kids Lifestyle</span></p>
+            <img src='https://assets.adidas.com/images/w_276,h_276,f_auto,q_auto,fl_lossy,c_fill,g_auto/ae163e593f614c9295e6addf0043b4c1_9366/B-Ball_Photo_Tee_White_HC2126_21_model.jpg' alt='shoes'></img>
+            <p>B-Ball Photo Tee <br></br><span>Men Lifestyle</span><br></br>new</p>
             <p className='dis-percentage'>-60%</p>
             <p className='discount-tag'><span className='org-price'>₹2499.00</span><span className='dis-price'>₹1000.00</span></p>
           </div>
           <div className='border'>
-            <img src='https://assets.adidas.com/images/w_276,h_276,f_auto,q_auto,fl_lossy,c_fill,g_auto/9b0887d84968406a8d94ad8e00f482d5_9366/adidas_Forta_Run_x_LEGO(r)_Baumhaus_Shoes_White_GZ0204_01_standard.jpg' alt='shoes'></img>
-            <p>adidas Forta Run x LEGO® <br></br>Baumhaus Shoes <br></br><span>Kids Running</span></p> 
+            <img src='https://assets.adidas.com/images/w_276,h_276,f_auto,q_auto,fl_lossy,c_fill,g_auto/adf2f7e02bac4bd5a637ae11016fb3f9_9366/Equipment_Support_93_Shoes_Gold_GX3893_01_standard.jpg' alt='shoes'></img>
+            <p>Equipment Support 93 Shoes <br></br><span>Men Originals</span><br></br>new</p>            
             <p className='dis-percentage'>-60%</p>
             <p className='discount-tag'><span className='org-price'>₹2499.00</span><span className='dis-price'>₹1000.00</span></p>
           </div>
           <div className='border'>
             <img src='https://assets.adidas.com/images/w_276,h_276,f_auto,q_auto,fl_lossy,c_fill,g_auto/d8b19f5114d24d878e72ad2600f615cd_9366/adidas_Forta_Run_x_LEGO(r)_VIDIYOtm_Shoes_Black_G57947_01_standard.jpg' alt='shoes'></img>
-            <p>adidas Forta Run x LEGO® VIDIYO™ <br></br>Shoes <br></br><span>Kids Running</span></p>
+            <p>SST Short Sleeve Tee <br></br><span>Men Originals</span><br></br>new</p>            
             <p className='dis-percentage'>-60%</p>
             <p className='discount-tag'><span className='org-price'>₹2499.00</span><span className='dis-price'>₹1000.00</span></p>
           </div>
           <div className='border'>
             <img src='https://assets.adidas.com/images/w_276,h_276,f_auto,q_auto,fl_lossy,c_fill,g_auto/62bd7dd25f334bfd992bad2600aa7d19_9366/adidas_Forta_Run_x_LEGO(r)_VIDIYOtm_Shoes_Black_G57946_01_standard.jpg' alt='shoes'></img>
-            <p>aadidas Forta Run x LEGO® VIDIYO™ <br></br>Shoes <br></br><span>Kids Running</span></p>
+            <p>Blocked 3-Stripes Tee <br></br><span>Men Lifestyle</span><br></br>new</p>            
             <p className='dis-percentage'>-60%</p>
             <p className='discount-tag'><span className='org-price'>₹2499.00</span><span className='dis-price'>₹1000.00</span></p>
           </div>
           <div className='border'>
             <img src='https://assets.adidas.com/images/w_276,h_276,f_auto,q_auto,fl_lossy,c_fill,g_auto/692dca8d91cc48098783acc9016fbcb3_9366/adidas_ZX_8000_x_LEGO(r)_Shoes_Black_GZ8216_01_standard.jpg' alt='shoes'></img>
-            <p>adidas ZX 8000 x LEGO® Shoes <br></br><span>Kids Lifestyle</span></p>
+            <p>Superstar Shoes <br></br><span>Men Lifestyle</span><br></br>online exclusive</p>            
             <p className='dis-percentage'>-60%</p>
             <p className='discount-tag'><span className='org-price'>₹2499.00</span><span className='dis-price'>₹1000.00</span></p>
           </div>
           <div className='border'>
             <img src='https://assets.adidas.com/images/w_276,h_276,f_auto,q_auto,fl_lossy,c_fill,g_auto/e7770d6427de4d1386b7ac38013a7491_9366/adidas_x_Classic_LEGO(r)_Bricks_Long_Sleeve_Fitted_Tee_Black_GN6778_01_laydown.jpg' alt='shoes'></img>
-            <p>adidas x Classic LEGO® Bricks Long <br></br>Sleeve Fitted Tee <br></br><span>Kids Lifestyle</span></p>
+            <p>Forum Low Shoes <br></br><span>Men Originals</span><br></br>new</p>            
             <p className='dis-percentage'>-60%</p>
             <p className='discount-tag'><span className='org-price'>₹2499.00</span><span className='dis-price'>₹1000.00</span></p>
           </div>
           <div className='border'>
             <img src='https://assets.adidas.com/images/w_276,h_276,f_auto,q_auto,fl_lossy,c_fill,g_auto/3260d91059974194af13adbb00ebc5a3_9366/adidas_x_LEGO(r)_Sport_Shoes_Red_H01503_01_standard.jpg' alt='shoes'></img>
-            <p>adidas x LEGO® Sport Shoes<br></br><span>Kids running</span></p>
+            <p>Na-Kel Message Tee(Gender Neutral) <br></br><span>Originals</span><br></br>online exclusive</p>            
             <p className='dis-percentage'>-60%</p>
             <p className='discount-tag'><span className='org-price'>₹2499.00</span><span className='dis-price'>₹1000.00</span></p>
           </div>
           <div className='border'>
             <img src='https://assets.adidas.com/images/w_276,h_276,f_auto,q_auto,fl_lossy,c_fill,g_auto/ed6a470260aa476aa488acc90186d093_9366/adidas_ZX_8000_x_LEGO(r)_Shoes_Yellow_H04832_01_standard.jpg' alt='shoes'></img>
-            <p>adidas ZX 8000 x LEGO® Shoes<br></br><span>Kids Lifestyle</span></p>
+            <p>Trefoil Tee <br></br><span>Men Lifestyle</span><br></br>new</p>            
             <p className='dis-percentage'>-60%</p>
             <p className='discount-tag'><span className='org-price'>₹2499.00</span><span className='dis-price'>₹1000.00</span></p>
           </div>
           <div className='border'>
             <img src='https://assets.adidas.com/images/w_276,h_276,f_auto,q_auto,fl_lossy,c_fill,g_auto/7e16603bc7c64e109acbad4900c10e7a_9366/adidas_x_Classic_LEGO(r)_Tee_Green_H26660_01_laydown.jpg' alt='shoes'></img>
-            <p>adidas x Classic LEGO® Tee<br></br><span>Kids Training</span></p>
+            <p>Graphic Stocked Fish Tee <br></br><span>Men Lifestyle</span><br></br>new</p>            
             <p className='dis-percentage'>-60%</p>
             <p className='discount-tag'><span className='org-price'>₹2499.00</span><span className='dis-price'>₹1000.00</span></p>
           </div>
           <div className='border'>
             <img src='https://assets.adidas.com/images/w_276,h_276,f_auto,q_auto,fl_lossy,c_fill,g_auto/0be395b38e4a43519bd1ad26008aecea_9366/adidas_x_LEGO(r)_Sport_Shoes_Black_FZ5438_01_standard.jpg' alt='shoes'></img>
-            <p>adidas x LEGO® Sport Shoes<br></br><span>Kids Running</span></p>
+            <p>Jehn Message Tee(Gender Neutral) <br></br><span>Skateboarding</span><br></br>online exclusive</p>            
             <p className='dis-percentage'>-60%</p>
             <p className='discount-tag'><span className='org-price'>₹2499.00</span><span className='dis-price'>₹1000.00</span></p>
           </div>
           <div className='border'>
             <img src='https://assets.adidas.com/images/w_276,h_276,f_auto,q_auto,fl_lossy,c_fill,g_auto/bf173c5ee1ac436094a7ad0c0047d94f_9366/adidas_x_LEGO(r)_Graphic_Tee_Black_GU8901_01_laydown.jpg' alt='shoes'></img>
-            <p>adidas x LEGO® Graphic Tee<br></br><span>Kids Lifestyle</span></p>
+            <p>Nora Message Tee(Gender Neutral) <br></br><span>originals</span><br></br>online exclusive</p>            
             <p className='dis-percentage'>-60%</p>
             <p className='discount-tag'><span className='org-price'>₹2499.00</span><span className='dis-price'>₹1000.00</span></p>
           </div>
@@ -128,7 +137,8 @@ export default function Kids() {
       <div className='flex-outside'> 
         <div className='flex-inside'>
           <div className='border'>
-            <img src='https://assets.adidas.com/images/w_276,h_276,f_auto,q_auto,fl_lossy,c_fill,g_auto/62bd7dd25f334bfd992bad2600aa7d19_9366/G57946_01_standard.jpg' alt="shoes"></img>
+            <video src='https://brand.assets.adidas.com/video/upload/q_auto,vc_auto,c_scale,w_0.5/video/upload/global%20brand%20publishing/Membership/Members%20Week/Launch%20Phase/brand-ss22-membersweek-launch-glp-m-teaser-large-hero-t.mp4' autoPlay="true" type='video/mp4'></video>
+            {/* <img src='https://assets.adidas.com/images/w_276,h_276,f_auto,q_auto,fl_lossy,c_fill,g_auto/62bd7dd25f334bfd992bad2600aa7d19_9366/G57946_01_standard.jpg' alt="shoes"></img> */}
             <p>adidas Forta Run x LEGO® VIDIYO™ Shoes <br></br><span>Performance</span></p>
             <p className='discount-tag'>₹4 9999.00</p>
           </div >
