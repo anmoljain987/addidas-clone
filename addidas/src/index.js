c59cb62aa015470639bced84fc71742084c4c36a
Anmol <anmoljain987@gmail.com>
2022-05-06 21:19:39 +0530

    changes

--- addidas/src/index.js
+++ addidas/src/index.js
@@ -4,17 +4,13 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-// import { Provider } from "react-redux";
-// import { store } from "./Store/index";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 ReactDOM.render(
   <React.StrictMode>
-    {/* // <Provider store={store}> */}
     <BrowserRouter>
       <App />
     </BrowserRouter>
-
-    {/* </Provider> */}
   </React.StrictMode>,
   document.getElementById("root")
 );
